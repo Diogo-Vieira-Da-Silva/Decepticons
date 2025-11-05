1f33bcb4a08667ee0fa40ed3f0b5071d231eb2e4
Diogo Vieira da Silva <diogovieira1903@gmail.com>
2025-11-05 11:18:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  let url = "http://localhost:3000/soldados"
 let nome = document.getElementById("nome")
-let email = document.getElementById("email")
+let modo_alternativo = document.getElementById("modo_alternativo")
 async function carregaDados(){
     await fetch(url)
     .then((response) => {
@@ -10,7 +10,7 @@ async function carregaDados(){
     .then((data) => {
         console.log(data)
         nome.innerHTML = data[0].nome
-        email.innerHTML = data[0].email
+        modo_alternativo.innerHTML = data[0].modo_alternativo
     })
     .catch((error) => {
         console.log("Erro no carregamento do banco de dados: " + error);
